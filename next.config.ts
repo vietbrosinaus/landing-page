c2e746e3a05aa1ee7707b0be5aa82e8951336e37
Bach Tran <tvbbd2@gmail.com>
2026-08-18 20:30:01 +0930

    Add temporary Discord redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/QChBwbDDVU",
+        permanent: false,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
